abbefa429a11a8c4d11d35fed8ce8618f333aa5e
GUSTAVO FERRARI CRISPIM NERI <gustavo_neri@estudante.sesisenai.org.br>
2025-04-29 12:31:42 -0300

    arrumei deu

--- script2.js
+++ script2.js
@@ -1,27 +1,27 @@
 const express = require('express');
 
-const porta = 8000
-const server = express()
-server.use(express.json())
+const porta = 8000;
+const server = express();
+server.use(express.json());
 
 server.get('/soma', (req, res) => {
-    console.log(req.query)
-    res.send('ok')
-})
+    console.log(req.query);
+    res.send('ok');
+});
 server.get('/subtracao', (req, res) => {
-    console.log(req.query)
-    res.send('ok')
-})
+    console.log(req.query);
+    res.send('ok');
+});
 
 server.get('/multiplicacao', (req, res) => {
-    console.log(req.query)
-    res.send('ok')
-})
+    console.log(req.query);
+    res.send('ok');
+});
 server.get('/divisao', (req, res) => {
-    console.log(req.query)
-    res.send('ok')
-})
+    console.log(req.query);
+    res.send('ok');
+});
 
 server.listen(porta, () => {
-    console.log(Servidor rodando na porta ${porta})
-})
+    console.log(`Servidor rodando na porta ${porta}`);
+});
